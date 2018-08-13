2f20341665acd0577020b7338d4dfa181efe8fbe
Dylan Bailey <dylanba1@gmail.com>
2018-08-12 22:56:26 -0500

    Prelude chapter of Javascript Allonge playground

---
+++ prelude.js
@@ -0,0 +1,12 @@
+let allonge = "Javascript " + " " + "Allonge"
+
+let elementary = 2 === 2;
+let elemString = 2 !== "2";
+
+let hi = 'hello' !== "goodbye"
+let testArray = [2+1, 2, 9*69];
+let ouroboros = [];
+ouroboros[0] = ouroboros;
+
+
+console.log(allonge);
