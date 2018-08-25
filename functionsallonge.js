1b583fd44920a84fe580c91c01d78786d6904531
Dylan Bailey <dylanba1@gmail.com>
2018-08-25 12:23:54 -0500

    Finished functions chapter

--- functionsallonge.js
+++ functionsallonge.js
@@ -39,4 +39,39 @@ void (2 + 2)
 
 // Return operator returns a value
 
-function () {return 'Hello ' + 'World '}
+function () {return 'Hello ' + 'World '};
+
+// Hello world with a function return statement
+
+function () {
+    return (function () {
+        return true
+    })
+};
+
+//Function that gives a function that returns true - nested
+
+// ** COMPOSITION ** \\
+
+function cookAndEat (food) {
+  return (cook(food));
+}
+
+function compose (a, b) {
+  return function (c) {
+    return a(b(c));
+  }
+}
+
+let cookAndEat = compose(eat, cook);
+
+// Functions are values that can be part of expressions, returned from other functions, and so forth.
+// Functions are reference values.
+// Functions are applied to arguments.
+// The arguments are passed by sharing, which is also called “pass by value.”
+// Function bodies have zero or more expressions.
+// Function application evaluates whatever is returned with the return keyword, or to undefined.
+// Function application creates a scope. Scopes are nested and free variable references closed over.
+// Variables can shadow variables in an enclosing scope.
+// let is an idiom where we create a function and call it immediately in order to bind values to names.
+// JavaScript uses var to bind variables within a function’s scope.
